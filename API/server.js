a0019364dd435da8e41610c277dd60316d9262a9
almulalic <almir.mulalic.am@gmail.com>
2020-04-13 17:38:42 +0200

    imporved folder structure, separated service from controller, fixed date bugs and improved error handling

--- API/server.js
+++ API/server.js
@@ -4,22 +4,24 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 
-// INITIAL DEFINITION
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.ts");
 
+// INITIAL DEFINITION
 const app = express();
 const port = process.env.PORT || 5000;
 
 // INITIALIZATION
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // CONTROLLERS IMPORT
+const IdentitiesController = require("./Controllers/IdentityController");
 
-const UsersController = require("./Controllers/UserControllers");
-
-app.use("/users", UsersController);
+// ACCESS POINTS
+app.use("/users", IdentitiesController);
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
