9d0721540c1d2a19a3c348a71bcf53e710142084
almulalic <almir.mulalic.am@gmail.com>
2020-04-26 16:26:16 +0200

    added token validation

--- src/Services/IdentityService/Enums/index.ts
+++ src/Services/IdentityService/Enums/index.ts
@@ -3,3 +3,4 @@ export { ConfirmationEnums } from "./ConfirmationEnums";
 export { ChangeConfirmationEnums } from "./ChangeConfirmationEnums";
 export { RegisterEnums } from "./RegisterEnums";
 export { ResendConfirmationEnums } from "./ResendConfirmationEnums";
+export { TokenValidationEnums } from "./TokenValidationEnums";
