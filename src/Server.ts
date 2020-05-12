12f1ad2ce99e1add2351a7bfab873239aa38d619
almulalic <almir.mulalic.am@gmail.com>
2020-05-12 14:18:11 +0200

    prisma fixes v2

--- src/Server.ts
+++ src/Server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
+import { PrismaClient } from "@prisma/client";
 
 import IdentityController from "./API/Controllers/IdentityController";
 import RestaurantController from "./API/Controllers/RestaurantController";
@@ -24,6 +25,21 @@ app.use("/identity", IdentityController);
 app.use("/restaurant", RestaurantController);
 app.use("/statistics", StatisticsController);
 app.use("/Leaderboard", LeaderboardController);
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers[0]);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
